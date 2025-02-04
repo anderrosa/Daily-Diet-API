300d74a09283c4041d823789607d07f1bc6b122b
Anderson Rosa <andersilvars7@gmail.com>
2025-02-04 18:52:09 -0300

    Adding and configuring database, updating .gitignore && requirements.txt

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, request, jsonify
+from models.user import Meal
+from database import db
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "your_secret_key"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:admin123@127.0.0.1:3306/daily_diet'
 
-@app.route("/refeicao", methods=['GET'])
-def meals():
-    return "Minha primeira refeição!"
+db.init_app(app)
+
+@app.route("/Meal", methods=['POST'])
+def create_meals():
+    return "Minha primeira refeiçao"
 
 if __name__ == '__main__':
     app.run(debug=True)
