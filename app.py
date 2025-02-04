40b13419eccfe11aa93072cd95f4ba4e50252e0b
Anderson Rosa <andersilvars7@gmail.com>
2025-02-04 19:38:56 -0300

    correction in database configuration

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from database import db
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "your_secret_key"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:admin123@127.0.0.1:3306/daily_diet'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 
--- database.py
+++ database.py
@@ -1,11 +1,3 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
-
-def init_app(app):
-    # Configurações do banco de dados
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql=pymysql://user:admin123@127.0.0.1:3306/daily_diet'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-    # Inicializa o banco de dados com a aplicação
-    db.init_app(app)
