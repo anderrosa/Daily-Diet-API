d93663d697b857387e50f5a433cbe1f32e95fba2
Anderson Rosa <andersilvars7@gmail.com>
2025-02-04 22:00:02 -0300

    creating user registration route

--- app.py
+++ app.py
@@ -1,17 +1,39 @@
 from flask import Flask, request, jsonify
-from models.user import Meal
+from models.user import Meal, User
 from database import db
+from flask_login import LoginManager, login_user, current_user, logout_user, login_required
+import bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "your_secret_key"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:admin123@127.0.0.1:3306/daily_diet'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+login_manager = LoginManager()
 db.init_app(app)
+login_manager.init_app(app)
+#view login
+login_manager.login_view = 'login'
+#Session <- conexão ativa
 
-@app.route("/Meal", methods=['POST'])
-def create_meals():
-    return "Minha primeira refeiçao"
+@login_manager.user_loader
+def load_user(user_id):
+  return User.query.get(user_id)
+
+@app.route('/user', methods=["POST"])
+def create_user():
+  data = request.json
+  username = data.get("username")
+  password = data.get("password")
+
+  if username and password:
+    hashed_password = bcrypt.hashpw(str.encode(password), bcrypt.gensalt())
+    user = User(username=username, password=hashed_password, role='user')
+    db.session.add(user)
+    db.session.commit()
+    return jsonify({"message": "Usuário cadastrado com sucesso!"})
+
+  return jsonify({"message": "Dados inválidos"}), 400
 
 if __name__ == '__main__':
     app.run(debug=True)
